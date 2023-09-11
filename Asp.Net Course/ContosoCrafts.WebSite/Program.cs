09fbd333314844d641d3cca4fd6d95a1c7df2a6a
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-11 11:47:56 +0200

    Added Service to read data from JSON file

--- Asp.Net Course/ContosoCrafts.WebSite/Program.cs
+++ Asp.Net Course/ContosoCrafts.WebSite/Program.cs
@@ -1,9 +1,11 @@
+using ContosoCrafts.WebSite.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddMvc().AddRazorRuntimeCompilation();
-
+builder.Services.AddTransient<JsonFileProductService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.

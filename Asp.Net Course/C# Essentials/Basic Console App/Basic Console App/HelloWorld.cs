8afbcc54cce56a73e2817593b426eb5c70ff54fc
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-13 15:10:20 +0200

    Build basic Structure for App + hello World

---
+++ Asp.Net Course/C# Essentials/Basic Console App/Basic Console App/HelloWorld.cs
@@ -0,0 +1,42 @@
+﻿namespace Basic_Console_App
+{
+    public class HelloWorld : iSubProgram
+    {
+        string programName;
+        string iSubProgram.ProgramName
+        {
+            get
+            {
+                return programName;
+            }
+            set
+            {
+                programName = value;
+            }
+        }
+
+        public static HelloWorld ReadProgram()
+        {
+            return (HelloWorld)new HelloWorld().Initialize();
+        }
+
+        public iSubProgram Initialize()
+        {
+            HelloWorld helloWorld = new HelloWorld();
+            helloWorld.programName = "Hello World";
+            return helloWorld;
+        }
+
+        public void Start()
+        {
+            string response;
+            Console.WriteLine("What is your name?");
+            response = Console.ReadLine();
+            Console.WriteLine($"Enjoy the course, {response}");
+
+            OperatingSystem thisOs = Environment.OSVersion;
+            Console.WriteLine(thisOs.Platform);
+            Console.WriteLine(thisOs.VersionString);
+        }
+    }
+}
---
+++ Asp.Net Course/C# Essentials/Basic Console App/Basic Console App/iSubProgram.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Console_App
+{
+    public interface iSubProgram
+    {
+        string ProgramName { get; internal set; }
+        public iSubProgram Initialize();
+        public void Start();
+    }
+}

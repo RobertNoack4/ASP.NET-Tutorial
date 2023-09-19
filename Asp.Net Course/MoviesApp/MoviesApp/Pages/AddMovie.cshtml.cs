b18d8bedfea969d8982b0442eb565c640bbc5f02
RobertNoack4 <77103810+RobertNoack4@users.noreply.github.com>
2023-09-19 11:10:21 +0200

    Added Data Binding for Adding Movies

--- Asp.Net Course/MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
+++ Asp.Net Course/MoviesApp/MoviesApp/Pages/AddMovie.cshtml.cs
@@ -1,12 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using MoviesApp.Data.Models;
 
 namespace MoviesApp.Pages
 {
     public class AddMovieModel : PageModel
     {
+        [BindProperty]
+        public Movie Movie { get; set; } 
+
         public void OnGet()
         {
+            Movie = new Movie
+            {
+                Title = string.Empty,
+                Description = string.Empty,
+                Rate = 0
+            };
+        }
+
+        public IActionResult OnPost()
+        {
+            string value = $"{Movie.Title} - {Movie.Rate} - {Movie.Description}";
+
+            if(!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            return Redirect("Movies");
         }
     }
 }
